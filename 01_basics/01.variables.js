742107aed36a822105853944f04cea186a61607c
musharrafzayb <65817770+musharrafzayb@users.noreply.github.com>
2024-06-23 05:43:09 +0000

    saved to console

---
+++ 01_basics/01.variables.js
@@ -0,0 +1,8 @@
+// people say write like this, write like that but accountId this is a good practice to write in coding world
+
+const accountId = 8682;
+let accountEmail = "musuah@gmail.com"
+var accountPassword = 8703409;
+accountCity = "Jaiput"
+
+console.log("accountId")
