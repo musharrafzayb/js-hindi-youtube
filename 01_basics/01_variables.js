1f815deff44f161e252f8dee327a15e55b5ba5c1
musharrafzayb <65817770+musharrafzayb@users.noreply.github.com>
2024-06-23 06:02:43 +0000

    ha ji

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,17 @@ const accountId = 8682;
 let accountEmail = "musuah@gmail.com"
 var accountPassword = 8703409;
 accountCity = "Jaiput"
+let state;
 
 // accountId = 2
-console.log("accountId")
+
+accountEmail = "hello@googl.com"
+accountPassword = 345
+accountCity = "patna"
+
+
+/* Prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, state])
