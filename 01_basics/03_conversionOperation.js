2ebf4b9149887a200f44b991ee9d4399d726ecac
musharrafzayb <65817770+musharrafzayb@users.noreply.github.com>
2024-06-23 10:21:25 +0000

    ventitre giugno 2024

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,1 +1,40 @@
-let score = 33
+let score = "true"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+Conversion
+"33" = 33 - this can be converted
+"33abc" = 🚫not converting implies => NaN
+true => 1; false => 0
+*/
+
+let isLoggedIn = "zareen"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+/* when, 
+1 => true; 0 => false
+"" => false; "Musharraf" => true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber)
+
+//***********************************************     Operations    **************************************
+
+let value = 3
+
+let negValue = -value
+
+console.log(negValue);
+
+console.log(8%3);
+console.log(2**3);
