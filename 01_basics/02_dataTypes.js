392e28d5f76e0caea249e83edf30bd8851e5ec22
musharrafzayb <65817770+musharrafzayb@users.noreply.github.com>
2024-06-23 06:32:13 +0000

    datatypes studied

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all the code as the newer version of javascript
+
+//alert(3 + 4) // we are using nodejs not browser so this code can't be used 
+//use "" double quotes, penso che it is good for readability. histesh also recommend
+
+//number => 2 to power 53
+//bigint => for the bigger number, mostly we don't use
+//string => ""
+//bolean => true or false
+//null => standalone value - null is a object it asked in interview
+//undefined => you have not defined t
+//symbol
+
+
+//object
+
+console.log(typeof "hello")
