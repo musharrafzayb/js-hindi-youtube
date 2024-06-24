909139d426715e8fe881c58316c5a6d9f94f1d59
musharrafzayb <65817770+musharrafzayb@users.noreply.github.com>
2024-06-24 06:05:42 +0000

    comparision

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// === checks strictly value as well it's datatypes
+console.log("2" === 2);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+
+console.log(undefined >= 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
